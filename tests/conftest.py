c7c64692a79c2aaf64dd78b2ece0b79813a38363
Maxim Rusin <Maxunris@gmail.com>
2024-01-23 20:19:51 +0300

    part 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 900
-    browser.config.window_height = 900
+    browser.config.window_width = 1000
+    browser.config.window_height = 1200
 
     yield
 
